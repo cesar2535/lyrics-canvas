017abe8e469ce6afc30b02a97ee5d16488eb62aa
Cesar Chen <cesar6134@gmail.com>
2014-12-10 15:29:38 +0800

    Prettified the js file

--- js/app/APP.js
+++ js/app/APP.js
@@ -1,4 +1,3 @@
-
 //Início de APP
 
 /**
@@ -6,20 +5,20 @@
  * O seu projeto JS em um objeto literal (JSON)
  * Sugerido por: Dennis Calazans
  * Veja mais em www.denniscalazans.com/js
- * 
- * Problema 
+ *
+ * Problema
  * Como modularizar o meu projeto, proteger a estrutura, tornar o código manutenível?
- * 
+ *
  * Solução
  * Criar apenas uma variável global e utilizar notação de objeto
- * 
+ *
  * Quando aplicar
  * Projetos pequenos - Como ferramenta para namespace.
  * Projetos médios - Como forma de agrupar códigos relacionados.
  * Projetos grandes e complexos - Como ferramenta de otimização  (Ver Pro JavaScript Design Patterns, capítulo 5).
  *
  * Algumas sugestões para organização:
- *  
+ *
  * Atributos - começam com _ e caixa baixa, segue com o padrão camelCase
  * APP._titulo = "Atributos começam com _";
  *
@@ -27,13 +26,13 @@
  * APP.retornarTitulo = function() {
  *	return this._titulo;
  * }
- * 
+ *
  * Módulos - começam com caixa alta, segue com padrão camelCase
  *  APP.Mudulo = {
- * 		_atributo: "valor" 
+ * 		_atributo: "valor"
  * }
- * 
- * Agradecimento a os meus alunos e amigos que vem trabalhando 
+ *
+ * Agradecimento a os meus alunos e amigos que vem trabalhando
  * e contribuindo para o desenvolvimento desta iniciativa:
  * Bruno Mota, Brenda, Ciclone, Pablo, Leonardo, Ricardo, Júlio Melo, Klayton
  **/
@@ -46,131 +45,132 @@ APP._nameSpace = "APP";
 
 //Inicia o APP
 APP.iniciar = function(Modulos) {
-	var quantidadeDeModulos = arguments.length;
+  var quantidadeDeModulos = arguments.length;
 
-	//Se algum Modulo for especificado, executa o setUp de App	
-	if(Modulos !== undefined && Modulos !== APP) {
-		APP.setUp();
-	}
+  //Se algum Modulo for especificado, executa o setUp de App	
+  if (Modulos !== undefined && Modulos !== APP) {
+    APP.setUp();
+  }
 
-	//Caso o Modulo seja omitido, TODOS os módulos de APP serão iniciados
-	if(quantidadeDeModulos == 0 || quantidadeDeModulos == 1) {
-		this.iniciarModulos(Modulos || APP);
-	} else if(quantidadeDeModulos > 1) {
-		APP.iniciarModulos.apply(this, arguments);
-	}
+  //Caso o Modulo seja omitido, TODOS os módulos de APP serão iniciados
+  if (quantidadeDeModulos == 0 || quantidadeDeModulos == 1) {
+    this.iniciarModulos(Modulos || APP);
+  } else if (quantidadeDeModulos > 1) {
+    APP.iniciarModulos.apply(this, arguments);
+  }
 };
 
 //Quando o APP for iniciado
-APP.setUp = function() {
-};
+APP.setUp = function() {};
 
 //Inicícia todos os módulos
 APP.iniciarModulos = function(Modulo) {
-	var Filho, i=0;
+  var Filho, i = 0;
 
-	if(arguments.length > 1) {
+  if (arguments.length > 1) {
 
-		for(;i<arguments.length;i++) {
-			APP.iniciarModulos(arguments[i]);
-		} 
-		return;
-	}
-
-
-	if(typeof Modulo != "object") return false;
-	
-	//Executa o método construtor do módulo, se existir
-	if(Modulo.hasOwnProperty('setUp') && typeof Modulo.setUp == "function") {
-		Modulo.setUp();
-	} else {
-		//Evita que durante a execução dos setUp, objetos inseridos na estrutura, 
-		//ampliem a verificação iterativa
-		return false;
-	}
-	
-	//Procura por Sub Módulos para fazer a mesma coisa
-	for(Filho in Modulo) {
-		
-
-		if(Modulo.hasOwnProperty(Filho) === true) {
-			if(Modulo[Filho] !== null && typeof Modulo[Filho] == "object") {
-				
-				//Permite a navegação em níveis superiores do namespace
-				Modulo[Filho]['pai'] = function() {
-					return Modulo;
-				};
+    for (; i < arguments.length; i++) {
+      APP.iniciarModulos(arguments[i]);
+    }
+    return;
+  }
 
 
-				//Cria em cada módulo a propriedade _nameSpace
-				//Ex: App.Contato.Formulario._nameSpace = App.Contato.Formulario
-				Modulo[Filho]['_nameSpace'] = (Modulo['_nameSpace'] || "APP") + '.'+Filho;
+  if (typeof Modulo != "object") return false;
 
-				
-				APP.iniciarModulos(Modulo[Filho]);
-			}
-		}
-	}
-	
-	return false;
+  //Executa o método construtor do módulo, se existir
+  if (Modulo.hasOwnProperty('setUp') && typeof Modulo.setUp == "function") {
+    Modulo.setUp();
+  } else {
+    //Evita que durante a execução dos setUp, objetos inseridos na estrutura, 
+    //ampliem a verificação iterativa
+    return false;
+  }
+
+  //Procura por Sub Módulos para fazer a mesma coisa
+  for (Filho in Modulo) {
+
+
+    if (Modulo.hasOwnProperty(Filho) === true) {
+      if (Modulo[Filho] !== null && typeof Modulo[Filho] == "object") {
+
+        //Permite a navegação em níveis superiores do namespace
+        Modulo[Filho]['pai'] = function() {
+          return Modulo;
+        };
+
+
+        //Cria em cada módulo a propriedade _nameSpace
+        //Ex: App.Contato.Formulario._nameSpace = App.Contato.Formulario
+        Modulo[Filho]['_nameSpace'] = (Modulo['_nameSpace'] || "APP") + '.' + Filho;
+
+
+        APP.iniciarModulos(Modulo[Filho]);
+      }
+    }
+  }
+
+  return false;
 };
 
 /**
  *	Retorna o valor da propriedade (nó) informado.
- *  Ex 1: 
+ *  Ex 1:
  * 		APP.nameSpace("APP.ModuloPai.ModuloFilho");
  * 		Retornará: APP.ModuloPai.ModuloFilho
- * 
- * 	Caso o nó seja um método, ele será executado.
- *	É possível passar parâmetros 
  *
- * 	Ex 2: APP.Calculadora.somar = function(a, b) { return a+b; }	
- * 		
+ * 	Caso o nó seja um método, ele será executado.
+ *	É possível passar parâmetros
+ *
+ * 	Ex 2: APP.Calculadora.somar = function(a, b) { return a+b; }
+ *
  *	APP.nameSpace("APP.Calculadora.somar", [7, 9]);
  * 		Retornará: 16
  */
 APP.nameSpace = function(nameSpace, arrayDeParametros) {
-	//Declaração de variáveis;
-	var no, nos, escopos, alvo, i;
+  //Declaração de variáveis;
+  var no, nos, escopos, alvo, i;
 
-		//Por comodidade, se for apenas 1 parâmetro,
-		 //pode passar direto, sem estar dentro de um array
-		//By Pablo
-		if(arrayDeParametros !== undefined && arrayDeParametros instanceof Array === false) {
-			arrayDeParametros = [arrayDeParametros];
-		}
+  //Por comodidade, se for apenas 1 parâmetro,
+  //pode passar direto, sem estar dentro de um array
+  //By Pablo
+  if (arrayDeParametros !== undefined && arrayDeParametros instanceof Array === false) {
+    arrayDeParametros = [arrayDeParametros];
+  }
 
-		//Escopos encontrados
-		escopos = [window];
+  //Escopos encontrados
+  escopos = [window];
 
-		//Nós do nameSpace
-		nos = nameSpace.split('.');
+  //Nós do nameSpace
+  nos = nameSpace.split('.');
 
-		//Para cada nó
-		for(i=0;i<nos.length;i++) {
-			no = nos[i];
+  //Para cada nó
+  for (i = 0; i < nos.length; i++) {
+    no = nos[i];
 
-			//Verifica se não é do protótipo
-			if(escopos[i].hasOwnProperty(no)) {
+    //Verifica se não é do protótipo
+    if (escopos[i].hasOwnProperty(no)) {
 
-				//Guarda o escopo encontrado
-				escopos.push(escopos[i][no]);
-			}
-		}
+      //Guarda o escopo encontrado
+      escopos.push(escopos[i][no]);
+    }
+  }
 
-		//Captura o alvo
-		alvo = escopos.pop();
+  //Captura o alvo
+  alvo = escopos.pop();
 
-		//Se a última referência encontrada for uma função
-		if(typeof alvo == 'function') {
+  //Se a última referência encontrada for uma função
+  if (typeof alvo == 'function') {
 
-			//A função é executada no escopo do alvo
-			return alvo.apply(escopos.pop(), arrayDeParametros || []);
+    //A função é executada no escopo do alvo
+    return alvo.apply(escopos.pop(), arrayDeParametros || []);
 
-		}
+  }
 
-		//Apenas retorna o alvo, caso não seja uma função
-		else { return alvo; }
+  //Apenas retorna o alvo, caso não seja uma função
+  else {
+    return alvo;
+  }
 }
 
 //FIM de APP
